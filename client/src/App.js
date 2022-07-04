fe5a0f8cbec79e38c28bc2ecce17d3c44348f7c5
Andres <garelan@gmail.com>
2022-07-03 22:52:39 -0500

    added missing code

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,24 @@ import Navbar from './components/Navbar';
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from 'apollo-boost';
 
-const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem('id_token');
-    
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    });
-  },
-  uri: '/graphql',
+const httpLink = createHttpLink({
+  uri: '/graphql',  
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({ 
+
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(), 
 });
 
 function App() {
